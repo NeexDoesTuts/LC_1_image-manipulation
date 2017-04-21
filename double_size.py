353d0c34a731193fa2504e405ef2821d7f1ad9c7
Neex.IO <neex-io@users.noreply.github.com>
2017-04-21 11:03:05 -0700

    Add double_size algorithm.

---
+++ double_size.py
@@ -0,0 +1,22 @@
+import image
+
+img = image.Image("luther.jpg")
+width = img.getWidth()
+height = img.getHeight()
+win = image.ImageWin(img.getWidth()*2, img.getHeight()*2)
+
+
+img_double = image.EmptyImage(width*2,height*2)
+
+for row in range(img.getHeight()):
+    for col in range(img.getWidth()):
+        pixel = img.getPixel(col, row)
+
+        c_pos = col * 2
+        r_pos = row * 2
+        img_double.setPixel(c_pos, r_pos, pixel)
+        img_double.setPixel(c_pos+1, r_pos, pixel)
+        img_double.setPixel(c_pos, r_pos+1, pixel)
+        img_double.setPixel(c_pos+1, r_pos+1, pixel)
+
+img_double.draw(win)
